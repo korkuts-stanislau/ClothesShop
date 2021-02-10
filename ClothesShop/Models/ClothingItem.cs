a4542f363ddae435673778ce828f40eef96da851
Stanislau <korkuc.stanislav@gmail.com>
2021-02-10 15:24:41 +0300

    Добавлен инициализатор базы.

--- ClothesShop/Models/ClothingItem.cs
+++ ClothesShop/Models/ClothingItem.cs
@@ -8,7 +8,7 @@ namespace ClothesShop.Models
     public class ClothingItem : Entity
     {
         public int TypeId { get; set; }
-        public ClothingItemType ClothingItemType { get; set; }
+        public ClothingItemType Type { get; set; }
 
         public int ManufacturerId { get; set; }
         public Manufacturer Manufacturer { get; set; }
@@ -16,7 +16,7 @@ namespace ClothesShop.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public int Size { get; set; }
-        public bool IsMail { get; set; }
+        public bool IsMale { get; set; }
         public decimal Price { get; set; }
     }
 }
--- ClothesShop/Startup.cs
+++ ClothesShop/Startup.cs
@@ -27,6 +27,8 @@ namespace ClothesShop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<DbInitializer>();
+
             services.AddDbContext<ClothesShopContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DbConnection")));
 
